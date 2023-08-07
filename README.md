cb92a04fab2a36886fdaa2d3a20eadc558c8ed71
miguelsndc <mglsndc@gmail.com>
2023-08-07 13:05:30 -0300

    gen

---
+++ README.md
@@ -0,0 +1,22 @@
+Pra usar isso aqui direito vc tem que:
+
+1 - jogar esse arquivo na pasta que vc quer q ele crie os arquivos
+
+2 - rodar no terminal da seguinte forma:
+
+    ´´´python 
+        python generate.py nome_da_pasta
+    ´´´
+
+    * se assegure de usar um nome válido, não suporto um milhão de exceções.
+
+3 - com isso ele vai gerar 3 arquivos:
+
+    1 - code.py
+    2 - input.txt
+    3 - output.txt
+
+O intuito é vc ler os inputs dentro de input.txt no arquivo code.py, e ele já vem com um
+snippet incluso que faz isso, use apenas um input comum pra pegar os dados.
+
+E no final das contas cuspir teus resultados no output.txt (use print(), afinal o output.txt se comporta como o stdout).
---
+++ generate.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+if len(sys.argv) > 2:
+    raise Exception('Only one argument is supported')
+elif len(sys.argv) == 1:
+    raise Exception('What u tryna generate n*gga ? Give me a name')
+
+
+folder_name = sys.argv[1]
+cur_dir = os.getcwd()
+path = os.path.join(cur_dir, folder_name)
+
+try:
+    os.mkdir(path)
+except OSError as error:
+    print(error)
+    exit(1)
+
+parent = os.path.basename(os.path.normpath(path))
+
+with open(os.path.join(path, 'code.py'), 'w') as f:
+    f.writelines([
+        'import sys\n',
+        f'sys.stdout = open(\'{parent}/output.txt\', \'w\')\n'
+        f'sys.stdin = open(\'{parent}/input.txt\', \'r\')\n'
+    ])
+
+    f.close()
+
+input_f = open(os.path.join(path, 'input.txt'), 'x')
+output_f = open(os.path.join(path, 'output.txt'), 'x')
